bc21f3c98b6c9a20e1f205268c06fa6cd90447f1
StarrFox <starrfox6312@gmail.com>
2020-08-26 21:16:35 -0500

    0.9.3: patch keyboard commands to allow window interaction

--- wizwalker/windows/keyboard.py
+++ wizwalker/windows/keyboard.py
@@ -11,21 +11,17 @@ class KeyboardHandler:
         self.window_handle = window_handle
 
     async def send_key(self, key, seconds):
+        # user32.PostMessageW(self.window_handle, 0x102, ord(key), 0)
         code = keycodes[key]
-        # https://docs.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-sendmessagew
-        # https://docs.microsoft.com/en-us/windows/win32/inputdev/wm-keydown
-        user32.PostMessageW(self.window_handle, 0x100, code, 0)
-        # Todo: find way to continuously send keys so people can still click in the game window
+        send_task = asyncio.create_task(self.key_send_task(code))
         await asyncio.sleep(seconds)
+        send_task.cancel()
         # https://docs.microsoft.com/en-us/windows/win32/inputdev/wm-keyup
         user32.PostMessageW(self.window_handle, 0x101, code, 0)
 
-    async def beta_send_key(self, key, seconds):
-        send_task = asyncio.create_task(self.key_send_task(key))
-        await asyncio.sleep(seconds)
-        send_task.cancel()
-
-    async def key_send_task(self, key):
+    async def key_send_task(self, code):
         while True:
-            user32.PostMessageW(self.window_handle, 0x102, ord(key), 0)
+            # https://docs.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-sendmessagew
+            # https://docs.microsoft.com/en-us/windows/win32/inputdev/wm-keydown
+            user32.PostMessageW(self.window_handle, 0x100, code, 0)
             await asyncio.sleep(0.1)
