58e1c99385ba04cecbcae201acf951ad441ac0b3
StarrFox <starrfox6312@gmail.com>
2021-02-22 12:53:42 -0600

    add wad sub-command and improve Wad class

--- wizwalker/__main__.py
+++ wizwalker/__main__.py
@@ -1,11 +1,13 @@
 import asyncio
 import sys
+from pathlib import Path
 
 import click
+import aiofiles
 from click_default_group import DefaultGroup
 from loguru import logger
 
-from wizwalker import WizWalker, utils
+from wizwalker import WizWalker, utils, Wad
 from wizwalker.cli import WizWalkerConsole
 
 logger.enable("wizwalker")
@@ -13,10 +15,11 @@ logger.remove(0)
 logger.add("wizwalker_debug.log", level="DEBUG", rotation="10 MB")
 
 
-@click.group(
-    help="Wizwalker cli", cls=DefaultGroup, default="cli", default_if_no_args=True,
-)
+@click.group(cls=DefaultGroup, default="cli", default_if_no_args=True)
 def main():
+    """
+    Wizwalker cli
+    """
     pass
     # app = WizWalker()
     #
@@ -26,8 +29,11 @@ def main():
     # app.run()
 
 
-@main.command(help="Start a cli instance")
+@main.command()
 def cli():
+    """
+    Start a cli instance
+    """
     if sys.platform != "win32":
         raise RuntimeError(f"This program is windows only, not {sys.platform}")
 
@@ -39,14 +45,14 @@ def cli():
     asyncio.run(_run_console())
 
 
-@main.command(help="Start (a/some) wizard101 instance(s)")
+@main.command()
 @click.option(
     "--instances", default=1, show_default=True, help="Number of instances to start"
 )
 @click.argument("logins", nargs=-1)
 def start_wiz(instances, logins):
     """
-    called when someone uses the "wiz" console command
+    Start multiple wizard101 instances and optionally login to them
     """
     if instances != len(logins) and instances != 1:
         click.echo("Not enough or too many logins for the number of instances")
@@ -54,18 +60,106 @@ def start_wiz(instances, logins):
 
     asyncio.run(utils.start_instances_with_login(instances, logins))
 
-    # utils.quick_launch()
-    #
-    # # speed is the name :sunglasses:
-    # import time
-    #
-    # # this probably isn't enough time for some computers
-    # time.sleep(7)
-    #
-    # handles = utils.get_all_wizard_handles()
-    # newest_handle = sorted(handles)[-1]
-    #
-    # utils.wiz_login(newest_handle, username, password)
+
+@main.group()
+def wad():
+    """
+    Wad manipulation commands
+    """
+    pass
+
+
+# TODO: finish
+@wad.command()
+def archive():
+    """
+    Create a wad from a directory
+    """
+    click.echo("Not implimented")
+
+
+@wad.command()
+@click.argument("input_wad", type=str)
+@click.argument(
+    "output_dir", type=click.Path(exists=True, file_okay=False), default="."
+)
+def unarchive(input_wad, output_dir):
+    """
+    Unarchive a wad into a directory
+
+    input_wad automatically fills in the rest of the path so you only need the name; i.e "root"
+    output_dir defaults to the current directory
+    """
+    wad_file = Wad(input_wad)
+    path = Path(output_dir)
+
+    if not path.exists():
+        if click.confirm(f"{path} does not exsist; create it?", default=True):
+            path.mkdir()
+        else:
+            exit(0)
+
+    async def _unarchive_wad():
+        # we don't use wad_file.unarchive so we can have this nice progress bar
+        with click.progressbar(
+            await wad_file.names(),
+            show_pos=True,
+            item_show_func=lambda i: i.split("/")[-1] if i else i,
+            show_eta=False,
+        ) as names:
+            for file_name in names:
+                dirs = file_name.split("/")
+                # not a base level file
+                if len(dirs) != 1:
+                    current = path
+                    for next_dir in dirs[:-1]:
+                        current = current / next_dir
+                        if not current.exists():
+                            current.mkdir()
+
+                file_path = path / file_name
+                file_data = await wad_file.get_file(file_name)
+
+                async with aiofiles.open(file_path, "wb") as fp:
+                    await fp.write(file_data)
+
+    asyncio.run(_unarchive_wad())
+
+
+@wad.command()
+@click.argument("input_wad", type=str)
+@click.argument("file_name")
+def extract(input_wad, file_name):
+    """
+    Extract a single file from a wad
+
+    input_wad automatically fills in the rest of the path so you only need the name; i.e "root"
+    """
+    wad_file = Wad(input_wad)
+
+    async def _extract_file():
+        try:
+            file_data = await wad_file.get_file(file_name)
+        except ValueError:
+            click.echo(f"No file named {file_name} found.")
+            exit(0)
+
+        relitive_file_name = file_name.split("/")[-1]
+
+        async with aiofiles.open(relitive_file_name, "wb+") as fp:
+            click.echo("Writing...")
+            await fp.write(file_data)
+
+    asyncio.run(_extract_file())
+
+
+# TODO: finish
+@wad.command()
+def insert():
+    """
+    Insert a single file into a wad
+    """
+    click.echo("Not implimented")
 
 
 if __name__ == "__main__":
--- wizwalker/wad.py
+++ wizwalker/wad.py
@@ -1,12 +1,24 @@
 import struct
 import zlib
 from collections import namedtuple
+from typing import Union
+from pathlib import Path
 
 import aiofiles
 
 from .utils import executor_function, get_wiz_install
 
 wad_file_info = namedtuple("wad_file_info", "offset, size, is_zip, crc, unzipped_size")
+
+
+class WadFileInfo:
+    def __init__(self, *, name, offset, size, is_zip, crc, unzipped_size):
+        self.name = name
+        self.offset = offset
+        self.size = size
+        self.is_zip = is_zip
+        self.crc = crc
+        self.unzipped_size = unzipped_size
 
 
 class Wad:
@@ -17,69 +29,106 @@ class Wad:
         self.name = name
         self.file_path = get_wiz_install() / "Data" / "GameData" / self.name
         if not self.file_path.exists():
-            raise RuntimeError(f"{self.name} not found.")
+            raise ValueError(f"{self.name} not found.")
 
-        self.journal = {}
-        self.refreshed_once = False
+        self._file_list = []
+        self._refreshed_once = False
+        self._open = False
+        self._file_pointer = None
 
     def __repr__(self):
         return f"<Wad {self.name=}>"
 
-    async def total_size(self):
-        """Total size of the files in this wad"""
-        total = 0
-        for file_info in self.journal.values():
-            total += file_info.size
+    async def size(self) -> int:
+        """
+        Total size of this wad
+        """
+        if not self._open:
+            await self.open()
 
-        return total
+        return sum(file.size for file in self._file_list)
+
+    async def names(self):
+        """
+        List of all file names in this wad
+        """
+        if not self._open:
+            await self.open()
+
+        return [file.name for file in self._file_list]
+
+    async def open(self):
+        # noinspection PyTypeChecker
+        self._file_pointer = open(self.file_path, "rb")
+        await self._refresh_journal()
+        self._open = True
+
+    def close(self):
+        self._file_pointer.close()
+        self._open = False
 
     # This method runs in a thread bc it is very slow
     @executor_function
-    def refresh_journal(self):
-        self.refreshed_once = True
+    def _refresh_journal(self):
+        if self._refreshed_once:
+            return
 
-        with open(self.file_path, "rb") as fp:
-            # KIWAD id string
-            fp.seek(5)
-            version = struct.unpack("<l", fp.read(4))[0]
-            file_num = struct.unpack("<l", fp.read(4))[0]
+        self._refreshed_once = True
 
-            if version >= 2:
-                fp.read(1)
+        fp = self._file_pointer
 
-            for _ in range(file_num):
-                offset = struct.unpack("<l", fp.read(4))[0]
-                size = struct.unpack("<l", fp.read(4))[0]
-                zsize = struct.unpack("<l", fp.read(4))[0]
-                is_zip = struct.unpack("?", fp.read(1))[0]
-                crc = struct.unpack("<l", fp.read(4))[0]
-                name_length = struct.unpack("<l", fp.read(4))[0]
-                name = (fp.read(name_length)).decode("utf-8")[:-1]
+        # KIWAD id string
+        fp.seek(5)
+        version = struct.unpack("<l", fp.read(4))[0]
+        file_num = struct.unpack("<l", fp.read(4))[0]
 
-                self.journal[name] = wad_file_info(
+        if version >= 2:
+            fp.read(1)
+
+        for _ in range(file_num):
+            offset = struct.unpack("<l", fp.read(4))[0]
+            size = struct.unpack("<l", fp.read(4))[0]
+            zsize = struct.unpack("<l", fp.read(4))[0]
+            is_zip = struct.unpack("?", fp.read(1))[0]
+            crc = struct.unpack("<l", fp.read(4))[0]
+            name_length = struct.unpack("<l", fp.read(4))[0]
+            name = (fp.read(name_length)).decode("utf-8")[:-1]
+
+            self._file_list.append(
+                WadFileInfo(
+                    name=name,
                     offset=offset,
                     size=size,
                     is_zip=is_zip,
                     crc=crc,
                     unzipped_size=zsize,
                 )
+            )
 
     async def get_file(self, name: str) -> bytes:
-        """Get the data contents of the named file"""
-        if not self.refreshed_once:
-            await self.refresh_journal()
+        """
+        Get the data contents of the named file
+        """
+        if not self._open:
+            await self.open()
 
-        target_file = self.journal.get(name)
+        target_file = None
+        for file in self._file_list:
+            if file.name == name:
+                target_file = file
 
         if not target_file:
-            raise RuntimeError(f"File {name} not found.")
+            raise ValueError(f"File {name} not found.")
 
         async with aiofiles.open(self.file_path, "rb") as fp:
             await fp.seek(target_file.offset)
             raw_data = await fp.read(target_file.size)
 
             if target_file.is_zip:
-                data = zlib.decompress(raw_data)
+                try:
+                    data = zlib.decompress(raw_data)
+                except zlib.error:
+                    data = raw_data
 
             else:
                 data = raw_data
@@ -88,15 +137,49 @@ class Wad:
 
     async def get_file_info(self, name: str) -> wad_file_info:
         """
-        Gets a wad_file_info for a file
-        this is the same as journal[name]
+        Gets a WadFileInfo for a named file
         """
-        if not self.refreshed_once:
-            await self.refresh_journal()
+        if not self._open:
+            await self.open()
 
-        try:
-            file_info = self.journal[name]
-        except KeyError:
-            raise RuntimeError(f"File {name} not found")
-        else:
-            return file_info
+        target_file = None
+        for file in self._file_list:
+            if file.name == name:
+                target_file = file
+
+        if not target_file:
+            raise ValueError(f"File {name} not found.")
+
+        return target_file
+
+    async def unarchive(self, path: Union[Path, str]):
+        if not path.exists():
+            raise ValueError(f"{path} does not exist.")
+
+        if not path.is_dir():
+            raise ValueError(f"{path} is not a directory.")
+
+        for file in self._file_list:
+            dirs = file.name.split("/")
+            # not a base level file
+            if len(dirs) != 1:
+                current = path
+                for next_dir in dirs[:-1]:
+                    current = current / next_dir
+                    if not current.exists():
+                        current.mkdir()
+
+            file_path = path / file.name
+            file_data = await self.get_file(file.name)
+
+            async with aiofiles.open(file_path, "wb") as fp:
+                await fp.write(file_data)
+
+    @classmethod
+    async def from_directory(self, path):
+        """
+        Create a Wad object from a directory
+
+        Args:
+            path: Path to directory to archive
+        """
