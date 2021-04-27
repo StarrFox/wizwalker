b9e5cb7549e08374fc31ffd369196d7d9ecc9acb
StarrFox <starrfox6312@gmail.com>
2021-04-26 20:35:51 -0500

    add quest teleporter example

---
+++ examples/README.md
@@ -0,0 +1,4 @@
+Examples of things you can do with wizwalker
+
+# quest teleporter
+teleports you to your quest when you press e
---
+++ examples/quest_teleporter.py
@@ -0,0 +1,32 @@
+import asyncio
+
+import wizwalker
+from wizwalker.hotkey import Hotkey, Listener
+
+
+class Quester:
+    def __init__(self):
+        self.walker = wizwalker.WizWalker()
+        self.walker.get_clients()
+
+    async def activate_hooks(self):
+        for client in self.walker.clients:
+            await client.activate_hooks("quest_struct", "player_struct")
+
+    async def handle_e_pressed(self):
+        for client in self.walker.clients:
+            quest_xyz = await client.quest_xyz()
+            await client.teleport(quest_xyz.x, quest_xyz.y, quest_xyz.z)
+
+    async def main(self):
+        await self.activate_hooks()
+
+        quest_hotkey = Hotkey(wizwalker.Keycode.E, self.handle_e_pressed)
+        quest_listener = Listener([quest_hotkey])
+
+        while True:
+            await quest_listener.listen()
+
+
+if __name__ == "__main__":
+    asyncio.run(Quester().main())
