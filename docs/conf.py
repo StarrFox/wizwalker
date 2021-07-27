4534909b7cdfa241f54f16d40df50c82ceb92901
StarrFox <starrfox6312@gmail.com>
2021-07-27 15:47:23 -0500

    switch back to rtd theme for docs

--- docs/conf.py
+++ docs/conf.py
@@ -33,10 +33,7 @@ with open("../pyproject.toml") as fp:
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
-    "sphinx.ext.autodoc",
-    "sphinx.ext.napoleon",
-]
+extensions = ["sphinx.ext.autodoc", "sphinx.ext.napoleon", "sphinx_rtd_theme"]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
@@ -52,24 +49,9 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = "amunra_sphinx_theme"
+html_theme = "sphinx_rtd_theme"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
-
-html_theme_options = {
-    # Title shown in the top left. (Default: ``project`` value.)
-    "navbar_title": "Wizwalker",
-    # Links to shown in the top bar. (Default: top-level ``toctree`` entries.)
-    "navbar_links": [
-        ("Quickstart", "quickstart"),
-        ("API", "api"),
-        ("Memory objects", "memory_objects"),
-        ("Index", "genindex"),
-    ],
-    "footer_text": "",
-    # If ``github_link`` is set, a GitHub icon will be shown in the top right.
-    "github_link": "https://github.com/StarrFox/Discord-chan",
-}
