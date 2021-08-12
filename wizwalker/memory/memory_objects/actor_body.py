da087b61024d89ed8971084b3e7c554e5cd6e898
Forrest <rmrocks01@gmail.com>
2021-08-12 17:54:15 -0500

    fix indentation

--- wizwalker/memory/memory_objects/actor_body.py
+++ wizwalker/memory/memory_objects/actor_body.py
@@ -118,7 +118,7 @@ class ActorBody(PropertyClass):
         """
         await self.write_value_to_offset(112, scale, "float")
         
-     async def model_update_scheduled(self) -> bool:
+    async def model_update_scheduled(self) -> bool:
         """
         If this body should have their model resynced with it's position
 
