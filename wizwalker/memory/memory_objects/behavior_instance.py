8134fbd7ba783d10d22392a1c630958b92878ee3
StarrFox <starrfox6312@gmail.com>
2021-09-17 08:42:19 -0500

    add BehaviorInstance.behavior_template

--- wizwalker/memory/memory_objects/behavior_instance.py
+++ wizwalker/memory/memory_objects/behavior_instance.py
@@ -1,4 +1,7 @@
+from typing import Optional
+
 from wizwalker.memory.memory_object import PropertyClass, DynamicMemoryObject
+from .behavior_template import DynamicBehaviorTemplate
 
 
 # TODO: 0x58 = behavior template
@@ -24,6 +27,18 @@ class BehaviorInstance(PropertyClass):
             behavior_template_name_id: The behavior template name to write
         """
         await self.write_value_to_offset(104, behavior_template_name_id, "unsigned int")
+
+    # note: hidden
+    async def behavior_template(self) -> Optional[DynamicBehaviorTemplate]:
+        """
+        Get this behavior's template
+        """
+        addr = await self.read_value_from_offset(0x58, "unsigned long long")
+
+        if not addr:
+            return None
+
+        return DynamicBehaviorTemplate(self.hook_handler, addr)
 
 
 class DynamicBehaviorInstance(DynamicMemoryObject, BehaviorInstance):
